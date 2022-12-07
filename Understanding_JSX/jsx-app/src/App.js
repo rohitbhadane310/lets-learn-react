68e3d5b77669a8a36aeed179efb7f0084880cf99
Rohit Bhadane <rohitbhadane310@gmail.com>
2022-12-07 20:47:43 +0530

    react-project

--- Understanding_JSX/jsx-app/src/App.js
+++ Understanding_JSX/jsx-app/src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
 
+
+let name = "Rohit";
 function App() {
   return (
     <>
-    <div className="blank">I am the programmer</div>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      
+    </nav>
+
+    <h1> Hello {name}</h1>
     </>
   );
 }
