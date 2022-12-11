80378dfae7becd0811624293c5245ee9dfa14199
Rohit Bhadane <rohitbhadane310@gmail.com>
2022-12-11 13:38:45 +0530

    make changes

---
+++ practice/practice-app/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../App.css';
+
+export default function Navbar(){
+    return(
+        <>
+        <div className="Nav">
+        <a href="/">Home</a>
+        <a href="/">about</a>
+        <a href="/">contact</a>
+        <a href="/">feedback</a>
+        </div>
+        </>
+    )
+}
