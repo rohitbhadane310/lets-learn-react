91bf98c38536f8e6d09913180df61d0f2a3fd0e5
Rohit Bhadane <rohitbhadane310@gmail.com>
2022-12-11 13:54:29 +0530

    make changes

--- practice/practice-app/src/components/Navbar.jsx
+++ practice/practice-app/src/components/Navbar.jsx
@@ -10,6 +10,16 @@ export default function Navbar(){
         <a href="/">contact</a>
         <a href="/">feedback</a>
         </div>
+
+        <div className="container">
+            <div className="heading">
+                <h1>This is my first website on react</h1>
+            </div>
+            <div className="para">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam impedit eius consequuntur odio, cumque illo itaque harum et unde nulla veritatis eaque expedita quam facere rem, totam accusamus, culpa voluptas.s</p>
+                <img src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg" alt="" />
+            </div>
+        </div> 
         </>
     )
 }
