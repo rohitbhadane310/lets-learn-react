42fcb9ce6c8d1239c6a13ceb58e52750fde0af70
Rohit Bhadane <rohitbhadane310@gmail.com>
2022-12-12 14:05:41 +0530

    update app

--- project-1/project-app/src/App.js
+++ project-1/project-app/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+import About from './components/About';
+// import Navbar from './components/Navbar';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-      <Navbar title = "textUtils" aboutText = "About textUtils"/>
+      {/* <Navbar title = "textUtils" aboutText = "About textUtils"/> */}
       <div className="container my-3">
-        <TextForm heading = "Enter the text to analyze below"/>
+        {/* <TextForm heading = "Enter the text to analyze below"/> */}
+        <About/>
       </div>
     </>
   );
