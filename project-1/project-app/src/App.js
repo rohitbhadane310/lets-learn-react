d9277efa98485a32d37628fffd44186fe6041f0e
Rohit Bhadane <rohitbhadane310@gmail.com>
2022-12-15 11:00:33 +0530

    update App

--- project-1/project-app/src/App.js
+++ project-1/project-app/src/App.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [Mode , setMode] = useState('dark');
+
+  
+
   return (
     <>
+    <Navbar title="TextUtils" aboutText = "About" mode = {Mode} />
       {/* <Navbar title = "textUtils" aboutText = "About textUtils"/> */}
       <div className="container my-3">
+      <div class="form-check form-switch text-dark">
+          <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+          <label class="form-check-label" for="flexSwitchCheckDefault">Enable Dark Mode</label>
+       </div>
         {/* <TextForm heading = "Enter the text to analyze below"/> */}
         <About/>
         <TextForm/>
--- project-1/project-app/src/components/Navbar.js
+++ project-1/project-app/src/components/Navbar.js
@@ -1,21 +1,23 @@
 import React from 'react'
+import '../App.css';
+
 // import { PropTypes } from 'react'
 
 export default function Navbar(props){
     return(
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className={`navbar navbar-expand-lg ${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">{props.title}</a>
+          <a className="navbar-brand" href="/" style={{color : "white"}}>{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className= "nav-link active" aria-current="page" href="/" >Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">{props.aboutText}</a>
+                <a className="nav-link" href="/" style={{color: "white"}}>{props.aboutText}</a>
              </li>
             </ul>
             <form className="d-flex" role="search">
