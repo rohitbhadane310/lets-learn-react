7196434ea8db08c24d956158cb83efc5d020f8b1
Rohit Bhadane <rohitbhadane310@gmail.com>
2022-12-16 20:25:32 +0530

    update app

--- project-1/project-app/src/components/Navbar.js
+++ project-1/project-app/src/components/Navbar.js
@@ -5,9 +5,9 @@ import '../App.css';
 
 export default function Navbar(props){
     return(
-        <nav className={`navbar navbar-expand-lg ${props.mode} bg-${props.mode}`}>
+        <nav className={`navbar navbar-expand-lg navbar-dark ${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/" style={{color : "white"}}>{props.title}</a>
+          <a className="navbar-brand" href="/" >{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -17,13 +17,9 @@ export default function Navbar(props){
                 <a className= "nav-link active" aria-current="page" href="/" >Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/" style={{color: "white"}}>{props.aboutText}</a>
+                <a className="nav-link" href="/">{props.aboutText}</a>
              </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
           </div>
         </div>
       </nav>
