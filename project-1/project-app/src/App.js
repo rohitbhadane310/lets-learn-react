2d4f664ea4fe15c06ee2eb44f49c47b2bcddc154
Rohit Bhadane <rohitbhadane310@gmail.com>
2022-12-18 21:06:42 +0530

    update app

--- project-1/project-app/src/App.js
+++ project-1/project-app/src/App.js
@@ -5,22 +5,18 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
-  const [Mode , setMode] = useState('dark');
+  const [Mode, setMode] = useState('dark');
 
-  
+
 
   return (
     <>
-    <Navbar title="TextUtils" aboutText = "About" mode = {Mode} />
+      <Navbar title="TextUtils" aboutText="About" mode={Mode} />
       {/* <Navbar title = "textUtils" aboutText = "About textUtils"/> */}
       <div className="container my-3">
-      <div class="form-check form-switch text-dark">
-          <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-          <label class="form-check-label" for="flexSwitchCheckDefault">Enable Dark Mode</label>
-       </div>
         {/* <TextForm heading = "Enter the text to analyze below"/> */}
-        <About/>
-        <TextForm/>
+        <About />
+        <TextForm />
       </div>
     </>
   );
