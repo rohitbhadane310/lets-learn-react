f99ad31f32ca3875c407cf76e223206179c7b003
Rohit Bhadane <rohitbhadane310@gmail.com>
2022-12-19 11:54:27 +0530

    update app

--- project-1/project-app/src/App.js
+++ project-1/project-app/src/App.js
@@ -1,20 +1,19 @@
 import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
 
 function App() {
-  const [mode, setMode] = useState('dark');
+  const [mode, setMode] = useState('light');
 
-  const toggleMode = () =>{
-    if(mode === 'light')
-    {
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = 'black';
     }
-    else
-    {
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
     }
@@ -22,12 +21,13 @@ function App() {
 
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode ={toggleMode}/>
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+      <Alert Alert ="This is alert"/>
       {/* <Navbar title = "textUtils" aboutText = "About textUtils"/> */}
       <div className="container my-3">
         {/* <TextForm heading = "Enter the text to analyze below"/> */}
         {/* <About /> */}
-        <TextForm mode={mode}/>
+        <TextForm mode={mode} />
       </div>
     </>
   );
