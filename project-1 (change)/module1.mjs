75c2ec82114608a86395af6934309ef82891941c
Rohit Bhadane <rohitbhadane310@gmail.com>
2022-12-26 19:25:28 +0530

    make change

--- project-1 (change)/module1.mjs
+++
@@ -1,7 +0,0 @@
-import d from './module2.mjs';
-import {a , b , c} from './module2.mjs';
-
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
--- project-1 (change)/module2.mjs
+++
@@ -1,8 +0,0 @@
-
-const a = "Rohit";
-const b = "Sumit";
-const c = "Rohan";
-const d = "Prinyaka";
-
-export default d;
-export {a , b , c};
